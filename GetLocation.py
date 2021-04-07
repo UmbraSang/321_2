358404131dc1e2fca1a50aabdf39b0629dbaf1af
daytondep <dayton_depradine@yahoo.ca>
2021-04-07 16:33:46 -0700

    Make the program activate on user interaction to mimic the action of submitting a report

--- GetLocation.py
+++ GetLocation.py
@@ -1,12 +1,20 @@
 import geocoder
+import keyboard  # using module keyboard
+
+#run pip instally geocoder, and pip install keyboard for the program to function as desired
 
 #This chunk of code intends to get the current longitude and latitude of whoever is using the system
 
 #Get location data from geocoder
+print("Welcome to Poseidon's CSN geolocation system! Press 'G' on your keyboard to get data on your current location")
 g = geocoder.ip('me')
 
-
-print(f"The country you are in is: {g.country}")
-print(f"The city you reside in is: {g.address}")
-print(f"Your latitude and longitude coordinates are: {g.latlng}")
-
+while True:  # making a loop
+    try:  # used try so that if user pressed other than the given key error will not be shown
+        if keyboard.is_pressed('g'):  # if key is pressed 
+            print(f"The country you are in is: {g.country}")
+            print(f"The city in which you are accessing this program: {g.address}")
+            print(f"Your latitude and longitude coordinates are: {g.latlng}")
+            break  # finishing the loop
+    except:
+        break  # if user pressed a key other than the given key the loop will break
