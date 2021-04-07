03de163de127ba59bb5e3094f089f5007f175b4d
daytondep <dayton_depradine@yahoo.ca>
2021-04-07 16:50:33 -0700

    Added test to ensure user is in Victoria

--- GetLocation.py
+++ GetLocation.py
@@ -10,12 +10,33 @@ print("Welcome to Poseidon's CSN geolocation system! Press 'G' on your keyboard 
 #Get location data from geocoder
 g = geocoder.ip('me')
 
+#Test that the location data returns data for our current location (Victoria, Canada)
+def locationtester(location):
+    testBoolean = True
+    if (location.country!="CA"):
+        testBoolean = False
+    
+    if(location.address!="Victoria, British Columbia, CA"):
+       testBoolean = False 
+    
+    if(location.latlng== None):
+        testBoolean = False
+    return testBoolean
+
+    
+
 while True:  
     try:  
         if keyboard.is_pressed('g'):  # if key is pressed 
-            print(f"The country you are in is: {g.country}")
-            print(f"The city in which you are accessing this program: {g.address}")
-            print(f"Your latitude and longitude coordinates are: {g.latlng}")
+            #location data will only print if user's location is in Victoria
+            if(locationtester(g)):
+                print(f"\nThe country you are in is: {g.country}")
+                print(f"The city in which you are accessing this program: {g.address}")
+                print(f"Your latitude and longitude coordinates are: {g.latlng}")
             break  
     except:
-        break  
+        break
+
+
+
+
